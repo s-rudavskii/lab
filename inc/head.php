5a0097e65fc5c12946cc6f7f74bc78244784d45c
Slava Rudavskii <s.rudavskii@magneticone.com>
2014-06-03 20:52:12 +0300

    add desing

--- inc/head.php
+++ inc/head.php
@@ -3,11 +3,23 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>Персональна лабораторія Слави Рудавського</title>
-  <link rel="stylesheet" type="text/css" href="/css/normalize.css" media="screen" />
-  <link rel="stylesheet" href="/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="/css/style.css" media="screen" />
-  <script src="/js/jquery.js" type="text/javascript"></script>
-  <script src="/js/bootstrap.min.js" type="text/javascript"></script>
+  <link rel="stylesheet" type="text/css" href="./css/normalize.css" media="screen" />
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="./css/style.css" media="screen" />
+  <script src="./js/jquery.js" type="text/javascript"></script>
+  <script src="./js/bootstrap.min.js" type="text/javascript"></script>
 </head>
 <body>
- 
+<div class="body-box">
+  <div class="left-menu">
+    <h1>Personal<span>Lab</span></h1>
+    <div class="nav">
+      <a class="item">Домашня</a>
+      <a class="item">Зміст</a>
+      <a class="item">Анотація</a>
+    </div>
+  </div
+  ><div class="right-content">
+
+  </div>
+</div>
