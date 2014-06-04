e63e3781030a2cd6d57a22c5cec125844ab66a2e
s.rudavskii <s.rudavskii@magneticone.com>
2014-06-05 01:27:14 +0300

    додав підтрику зКліп та виправив помилки в роботі скріпта форматування

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-var z_type = 1;
-
-setTimeout(z_rep, 2000);
+setTimeout(z_rep, 100);
 
 function z_rep(){
   var source = $('#source').val();
@@ -8,15 +6,42 @@ function z_rep(){
   var zm = source + '\n';
   zm = zm.replace(/…/g, '...');
   zm = zm.replace(/([¬])/g, '');
-  zm = zm.replace(/([\t]){2,}/g, ' ');
+  zm = zm.replace(/([\t])/g, ' ');
   zm = zm.replace(/([ ]){2,}/g, ' ');
-  zm = zm.replace(/([^\. \w])+([\s\d\r\n])*([\r\n])([\s])*/g, '\n');
+  zm = zm.replace(/([\s])*([\.])+([\s\d\r\n\.])*([\r\n])([\s])*/g, '\n');
   zm = zm.replace(/([\s\.\r\n])*([\r\n])([\s])*/g, '\n');
-  if(z_type == 1) {
-    zm = zm.replace(/([-])+([\r\n])([\s])*/g, '');
+  if($('#btn-0').hasClass('active')) {
+    zm = zm.replace(/([-])+([\r\n])/g, '');
   }
-  zm = zm.replace(/([\r\n])*$/g, '');
+  if($('#btn-1').hasClass('active')) {
+    zm = zm.replace(/([\d\s])+([\r\n])/g, '\n');
+  }
+  zm = zm.trim();
+  if($('#btn-2').hasClass('active')) {
+    var zma = zm.split('\n');
+    zm = '';
+    zma.forEach(function(string){
+      zm = zm + string.substr(0,1).toUpperCase()+string.substr(1)+'\n';
+    })
+  }
 
   $('#target').val(zm);
-  setTimeout(z_rep, 500);
+  setTimeout(z_rep, 1000);
 }
+
+function un(){
+  $('#copy').text('В буфер');
+}
+
+$(document).ready(function(){
+  $("#copy").zclip({
+    path: "./inc/ZeroClipboard.swf",
+    copy: function(){
+      return $('#target').val();
+    },
+    afterCopy: function(){
+      $('#copy').text('Готово');
+      setTimeout(un,1000)
+    }
+  });
+});
