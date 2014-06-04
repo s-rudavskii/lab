6fbb4de91039637c9b41182944622c656f3ddaea
s.rudavskii <s.rudavskii@magneticone.com>
2014-06-05 01:27:02 +0300

    додав підтрику зКліп та виправив помилки в роботі скріпта форматування

--- inc/zmist.php
+++ inc/zmist.php
@@ -2,12 +2,21 @@
   <small>Використовується для форматування зміту,
     вирізає номери сторінок та забирає усі непотрібні символи</small></h1>
 <div class="btn-toolbar">
-  <div class="btn-group left25" data-toggle="buttons-radio">
-    <button type="button" class="btn">Не змінювати</button>
-    <button type="button" class="btn active" title="Знімає перенесення стрічки якщо перший символ в стрічці нижнього реєстру">Перенесення стрічок</button>
-    <button type="button" class="btn" title="Перша буква в кожній стрічці буде замінена на аналогічну у великому реєстрі">Перше слово з великої літери</button>
+  <div class="btn-group left25" data-toggle="buttons-checkbox">
+    <button type="button" id="btn-0" class="btn active"
+            title="Знімає перенесення стрічки якщо перший символ в стрічці нижнього реєстру">
+      Перенесення стрічок</button>
+    <button type="button" id="btn-1" class="btn"
+            title="Видаляє усі цифри в кінці стрічки (не рекомендується)">
+      Форс видалення</button>
+    <button type="button" id="btn-2" class="btn"
+            title="Перша буква в кожній стрічці буде замінена на аналогічну у великому реєстрі">
+      Перше слово з великої літери</button>
   </div>
-  <button type="button" class="btn btn-link">Завантажити XML</button>
+  <button type="button" id="copy" style="width: 100px;" class="btn btn-primary"
+          title="Копіювати у буфер обміну">В буфер</button>
+  <button type="button" class="btn btn-link"
+          title="Спеціально для маленької Лисички ^_^">Завантажити XML</button>
 </div>
 <div class="left25 top25">
   <textarea id="source" cols="30" rows="10"></textarea>
