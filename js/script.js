e0667b2e4acf17c97c08a20a1dcd8873b63adac9
s.rudavskii <s.rudavskii@magneticone.com>
2014-06-05 10:30:15 +0300

    додав скріпт для генерування хмл

--- js/script.js
+++ js/script.js
@@ -10,11 +10,11 @@ function z_rep(){
   zm = zm.replace(/([ ]){2,}/g, ' ');
   zm = zm.replace(/([\s])*([\.])+([\s\d\r\n\.])*([\r\n])([\s])*/g, '\n');
   zm = zm.replace(/([\s\.\r\n])*([\r\n])([\s])*/g, '\n');
-  if($('#btn-0').hasClass('active')) {
-    zm = zm.replace(/([-])+([\r\n])/g, '');
-  }
   if($('#btn-1').hasClass('active')) {
     zm = zm.replace(/([\d\s])+([\r\n])/g, '\n');
+  }
+  if($('#btn-0').hasClass('active')) {
+    zm = zm.replace(/([-])+([\r\n])/g, '');
   }
   zm = zm.trim();
   if($('#btn-2').hasClass('active')) {
@@ -22,11 +22,33 @@ function z_rep(){
     zm = '';
     zma.forEach(function(string){
       zm = zm + string.substr(0,1).toUpperCase()+string.substr(1)+'\n';
-    })
+    });
   }
 
   $('#target').val(zm);
   setTimeout(z_rep, 1000);
+}
+
+function z_xml(){
+  var target = $('#target').val();
+  var title = $('#title').val();
+  var xml = '<?xml version="1.0" encoding="utf-8" ?>' +
+    '<data>' +
+    '<setup source="" next="" checkCount="0" autoLoadTime="1" taskTime="0">' +
+    '  <book toolHide="0" fullScreen="false" hideTitle="false" hideObjects="false" margin="10" zoomMin="0.5" zoomMax="2" zoomStep="0.1">' +
+    '    <content title="' + title + '" >' +
+    '      <item text="" color="FFFFFF" lMargin="220" size="40"/>' +
+    '      <item page="1" text="Обкладинка" color="000066" lMargin="0" size="35" />' +
+    '      <item page="4" text="Зміст" color="000066" lMargin="0" size="35" />';
+  target.forEach(function(string){
+    xml = xml + '\n      <item page="" text="' + string +
+      '" color="000066" lMargin="0" size="35" />';
+  });
+  xml = xml + '\n    </content>' +
+    '  </book>' +
+    '</setup>' +
+    '</data>';
+
 }
 
 function un(){
